7deac81e75e88fa3a4066e51860124567cd6365e
unknown <williamson.silva@gmail.com>
2021-03-22 18:18:59 -0300

    primeiros passos de JS

---
+++ js/main.js
@@ -0,0 +1,63 @@
+
+var d = new Date();
+alert(d);
+
+
+
+
+/* 
+var count;
+for (count=0; count<=5; count++){
+    alert(count);
+}; */
+
+
+
+/*var count = 0;
+while(count <= 5){
+    console.log (count);
+    //count = count +1;
+    alert(count);
+    count++;
+};
+*/
+
+/*
+var idade = prompt("Qual a sua idade?");
+var idade = 18;
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};*/
+
+//var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+//console.log(frutas);
+//alert(frutas[1].nome);
+
+//var fruta = {nome:"maçã", cor:"vermelha"};
+// console.log(fruta.nome);
+//alert(fruta.cor);
+
+//var lista = ["maça", "pêra", "laranja"];
+//console.log(lista [1]);
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+//alert(lista [1]);
+//console.log (lista.reverse()); // trazer os elementos contrários na minha lista
+//console.log (lista);
+//console.log (lista.toString());
+
+//console.log (lista.join (" -"));
+// var nome = "Williamson Silva"; /* ele é de tipagem dinâmica, ele já vai saber o tipo da string*/
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mundo";
+// alert(nome + " tem " +  idade + " anos");
+// alert(idade + idade2);
+
+//console.log(nome);
+//console.log(n1 * n2);
+// console.log(frase.toUpperCase()); tudo maiúsculo
+//console.log(frase.toLowerCase());
